4ffe0c06db4cc9269005a5258485ae460618ab3a
Rodrigo GL <gonlairo@gmail.com>
2019-02-13 23:37:13 -0800

    first five questions

--- hw4.sh
+++ hw4.sh
@@ -1,18 +1,31 @@
-DATAFILE="/Users/rodrigo/Desktop/bash_hw4/transaction.zip"
-
-# first question
+DATAFILE=""
 
 unzip -p ${DATAFILE} |
     head -n1 |
     tr ',' '\n' |
     nl > column_index.txt
     
+unzip -p ${DATAFILE} |
+    cut -d , -f1-61 |
+    awk '{ print length }' |
+    sort -n -r |
+    head -n 1 > bicycle.csv > maxchars.txt
+
+unzip -p ${DATAFILE} |
+    cut -d , -f1-61 |
+    grep -i 'bicycle' > bicycle.csv
 	 
 unzip -p ${DATAFILE} |
     cut -d , -f 18 |
     sort -n |
-    awk 'NF'
+    awk 'NF' |
     uniq > funding_agency_set.txt
 
-wc -l funding_agency_set.txt
+transaction_description=8
+total_obligation=25
+
+unzip -p ${DATAFILE} |
+    cut -d , -f${transaction_description},${total_obligation} |
+    sort -n -r |
+    head -n 3 > largest.csv
     
